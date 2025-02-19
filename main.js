f72d0c1b58dcf4dc879f65e9d627e6661e7279f2
MoatazZewin <moatazzewin@gmail.com>
2025-02-19 15:27:08 +0200

    add projects

--- main.js
+++ main.js
@@ -6,15 +6,50 @@ btnToggle.addEventListener("click", () => {
   headerList.classList.toggle("show");
 });
 
-const swiper = new Swiper(".swiper", {
-  direction: "horizontal",
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 10,
+// const swiper = new Swiper(".swiper", {
+//   direction: "horizontal",
+//   loop: true,
+//   slidesPerView: 1,
+//   spaceBetween: 10,
 
-  // Pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+//   autoplay: {
+//     delay: 2000, // Auto-slide every 3 seconds
+//     disableOnInteraction: false, // Keep autoplay after interaction
+//     reverseDirection: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next", // Next button
+//     prevEl: ".swiper-button-prev", // Previous button
+//   },
+// });
+
+let headerSection = document.getElementsByTagName("header")[0];
+let anchorLink = [...document.querySelectorAll("ul#header-list li a")];
+let buttonSpan = document.querySelector("header button span");
+let ul = document.getElementById("header-list");
+
+let lang = document.querySelector("header .lang");
+
+window.addEventListener("scroll", () => {
+  if (+window.scrollY > 0) {
+    headerSection.classList.add("sticky");
+    anchorLink.map((e) => {
+      e.classList.add("anchor-color");
+    });
+    buttonSpan.classList.add("anchor-color-2");
+    buttonSpan.style.setProperty("--after-color", "white");
+    ul.style.setProperty("--color-scroll", "white");
+    lang.classList.add("scroll");
+  } else {
+    headerSection.classList.remove("sticky");
+    anchorLink.map((e) => {
+      e.classList.remove("anchor-color");
+    });
+    buttonSpan.classList.remove("anchor-color-2");
+    buttonSpan.style.setProperty("--after-color", "rgb(117 27 31)");
+    ul.style.setProperty("--color-scroll", "rgb(117 27 31)");
+    lang.classList.remove("scroll");
+  }
 });
+
+AOS.init();
